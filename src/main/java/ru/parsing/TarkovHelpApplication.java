a82e55d1c6c28d3b2c548f95a528d613ed3ce31d
alvator92 <alvator@list.ru>
2023-01-10 18:42:05 +0300

    add depencies

--- src/main/java/ru/parsing/TarkovHelpApplication.java
+++ src/main/java/ru/parsing/TarkovHelpApplication.java
@@ -1,4 +1,13 @@
 package ru.parsing;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class TarkovHelpApplication {
+    public static void main (String... args) {
+        SpringApplication.run(TarkovHelpApplication.class, args);
+
+    }
+
 }
